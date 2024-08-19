b5d0252d9b0ce4dc16f4ad623ea0b8671b37e09f
anna.mitelkova <anna.mitelkova@latoken.com>
2024-08-19 18:33:31 +0200

    kinda works

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKe
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from dotenv import load_dotenv
 import os
+import logging
 
 
 # load vars from .env file
@@ -15,6 +16,14 @@ user_state = {}
 # list of ingredients, that user has and prints in bot
 ingredients = []
 
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send an introduction to a new user"""
@@ -29,15 +38,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # add menu
     # buttons
     keyboard = [
-        ["Wanna cocktail", "Stop"],
-        ["Help"]
+        ["Wanna cocktail🍹", "Stop"],
+        ["Help - mock"] # mock now
     ]
     
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
 
-    # welcome-text + menu
+    # welcome-text + menu(keyboard)
     await update.message.reply_text(
-        'Welcome to the Cocktail Bot! Choose an option:', 
+        'Welcome to the Cocktail Bot! Choose an option and press the button', 
         reply_markup=reply_markup
     )
 
@@ -53,13 +62,13 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await context.bot.send_message(chat_id=query.message.chat_id, text="Wanna cocktail? Gimme ingredients")
         user_state[chat_id] = 'waiting_for_ingredients'
 
-
-async def collect_ingredients(update) -> None:
+async def collect_ingredients(update, context) -> None:
     """Collect ingredients form the user message if 'waiting_for_ingredients' state is active."""
     chat_id = update.effective_chat.id
     
     if user_state.get(chat_id) == 'waiting_for_ingredients':
         ingredients.append(update.message.text)
+        await update.message.reply_text(f"Added: {update.message.text}") # to check
 
 async def wanna_cocktail(update, context) -> None:
     """Resend you formatted ingredients with additional text"""
@@ -110,6 +119,17 @@ def main():
 
     # stop_handler = CommandHandler('stop', stop_waiting)
     # application.add_handler(stop_handler)
+
+    # menu button handler
+    # Regex must fully match!!!
+    wanna_handler = MessageHandler(filters.TEXT & filters.Regex('^Wanna cocktail🍹$'), wanna_cocktail)
+    application.add_handler(wanna_handler)
+
+    collect_handler = MessageHandler(filters.TEXT & ~filters.COMMAND & ~filters.Regex('^Stop$'), collect_ingredients)
+    application.add_handler(collect_handler)
+
+    stop_handler = MessageHandler(filters.TEXT & filters.Regex('^Stop$'), stop_waiting)
+    application.add_handler(stop_handler)
 
     # start working until ctrl+C
     application.run_polling()
