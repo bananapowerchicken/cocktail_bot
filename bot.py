c48f6cbc2e8227f3679e0f9c838ecf50507ba623
anna.mitelkova <anna.mitelkova@latoken.com>
2024-08-19 16:21:17 +0200

    added kinda proto menu

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, CallbackQueryHandler
 from dotenv import load_dotenv
 import os
@@ -19,23 +19,39 @@ ingredients = []
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send an introduction to a new user"""
 
-    # Создаем кнопку    
+    # # Create a button connected to msg   
+    # keyboard = [
+    #     [InlineKeyboardButton("Wanna cocktail", callback_data='wanna_cocktail')]
+    # ]
+    # reply_markup = InlineKeyboardMarkup(keyboard)
+    # await update.message.reply_text('Welcome to the Cocktail Bot! Click the button below to get started.', reply_markup=reply_markup)
+
+    # add menu
+    # buttons
     keyboard = [
-        [InlineKeyboardButton("Wanna cocktail", callback_data='wanna_cocktail')]
+        ["Wanna cocktail", "Stop"],
+        ["Help"]
     ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    # await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! I am a cocktail bot!")
-    await update.message.reply_text('Welcome to the Cocktail Bot! Click the button below to get started.', reply_markup=reply_markup)
+    
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+
+    # welcome-text + menu
+    await update.message.reply_text(
+        'Welcome to the Cocktail Bot! Choose an option:', 
+        reply_markup=reply_markup
+    )
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handle button clicks."""
     query = update.callback_query
     await query.answer()
 
-    # Определяем, какая кнопка была нажата
+    # Detection of pressed btn
     if query.data == 'wanna_cocktail':
-        # Выполняем логику, которая была бы в обработчике команды /wanna
+        # execute logic from handler /wanna (how not to duplicate?)
+        chat_id = update.effective_chat.id
         await context.bot.send_message(chat_id=query.message.chat_id, text="Wanna cocktail? Gimme ingredients")
+        user_state[chat_id] = 'waiting_for_ingredients'
 
 
 async def collect_ingredients(update) -> None:
@@ -80,20 +96,20 @@ def main():
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
 
-    # # Создаем обработчик сообщений, который будет вызывать функцию echo при получении любого текста
-    # echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
-    # application.add_handler(echo_handler)
+    # # # Создаем обработчик сообщений, который будет вызывать функцию echo при получении любого текста
+    # # echo_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
+    # # application.add_handler(echo_handler)
 
-    # Callback handler for buttons
-    button_handler = CallbackQueryHandler(button)
-    application.add_handler(button_handler)
+    # # Callback handler for buttons
+    # button_handler = CallbackQueryHandler(button)
+    # application.add_handler(button_handler)
 
-    # trial wanna_cocktail handler
-    wanna_handler = CommandHandler('wanna', wanna_cocktail)
-    application.add_handler(wanna_handler)
+    # # trial wanna_cocktail handler
+    # wanna_handler = CommandHandler('wanna', wanna_cocktail)
+    # application.add_handler(wanna_handler)
 
-    stop_handler = CommandHandler('stop', stop_waiting)
-    application.add_handler(stop_handler)
+    # stop_handler = CommandHandler('stop', stop_waiting)
+    # application.add_handler(stop_handler)
 
     # start working until ctrl+C
     application.run_polling()
