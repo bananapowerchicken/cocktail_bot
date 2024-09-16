407aab17939f5a7aa84e1d7238b5c5b4b1a31f8b
Анна Мителькова <221882@mail.ru>
2024-09-16 13:55:04 +0200

    add request for all ingredients

--- v2-aiogram/app/database/requests.py
+++ v2-aiogram/app/database/requests.py
@@ -1,4 +1,12 @@
 # requests to DB
 
-from app.database.models import Recipe
+from app.database.models import Ingredient, async_session
+from sqlalchemy import select
+
+
+async def get_ingredients():
+    async with async_session() as session:
+        res = await session.execute(select(Ingredient))
+        ingredients = res.scalars().all()
+        return ingredients
 
